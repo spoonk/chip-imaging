b19da24d967669ff92dc25fa392fb49ed7fdce56
spoonk <cornwlucas@gmail.com>
2023-04-03 08:51:46 -0700

    start backend environment code

---
+++ backend/server.py
@@ -0,0 +1,14 @@
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello():
+    return 'Hello cruel world!'
+
+@app.route('/echo/<phrase>')
+def echo(phrase):
+    return [phrase]
+
+if __name__ == '__main__':
+    app.run(host='127.0.0.1', port=8080, debug=True)

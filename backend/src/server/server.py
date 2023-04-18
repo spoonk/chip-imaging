11aae8d75d1756f45297c8e1eb3240f4f6c43ecb
Brae-pete <cornwlucas@gmail.com>
2023-04-18 15:31:26 -0700

    going to start testing camera feed

--- backend/src/server/server.py
+++ backend/src/server/server.py
@@ -1,14 +1,18 @@
-from flask import Flask
+from flask import Flask, Response
 app = Flask(__name__)
-
 
 @app.route('/')
 def hello():
-    return 'Hello cruel world!'
+    return 'Hello, cruel world!'
 
 @app.route('/echo/<phrase>')
 def echo(phrase):
     return [phrase]
 
+
+@app.route("/camera_feed")
+def stream_camera_feed():
+    return Response()
+
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=8080, debug=True)
+    app.run(host='127.0.0.1', port=8078, debug=True)

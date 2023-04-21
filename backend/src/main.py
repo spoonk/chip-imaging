db3619756e4f3a69e6e0d97fab498cb0e522793f
spoonk <cornwlucas@gmail.com>
2023-04-21 11:52:30 -0700

    I think I need to uninstall those packages from earlier

--- backend/src/main.py
+++ backend/src/main.py
@@ -15,52 +15,34 @@ from PIL import Image
 from camera.pmm_camera import PMMCamera
 
 app = Flask('src')
-# CORS(app)
+CORS(app)
 sock = SocketIO(app, cors_allowed_origins='*')
 
-cache = {} # server lifetim-wide cache
-
-@app.route('/initCamera')
-def init_camera():
-    cache['camera'] = PMMCamera()
-    cache['camera'].connect()
-    cache['camera'].set_gain(1)
-    logging.getLogger().info("camera initialized")
-    return "worked!"
+cache = {} # server lifetime-wide cache
 
 @sock.on('video')
 def handle_video():
-    # cache['camera'] = PMMCamera()
-    # cache['camera'].connect()
-    # cache['camera'].set_gain(1)
-    # logging.getLogger().info("camera initialized")
-    # init_camera()
-    logging.getLogger().info("starting video loop")
-    while True:
-        # sock.emit('message', i)
-        # cam = cache['camera']
-        # sock.emit('message', 'dddddd')
-        sock.send('hi')
+    # cam = PMMCamera()
+    # cam.connect()
 
+    logging.getLogger().info("starting video loop")
+    i = 0
+    for i in range(201):
+        sock.emit('message', i)
         # im = Image.fromarray(cam.take_image())
         # img_byte_arr = io.BytesIO()
         # im.save(img_byte_arr, format='PNG')
         # img_byte_arr = img_byte_arr.getvalue()
         # sock.emit('frame', {'image_data': img_byte_arr})
         sleep (0.1)
-        # logging.getLogger().info("sent image")
 
 @sock.on('connect')
 def connect():
     print('connected')
 
-
 @sock.on('disconnect')
 def connect():
     print('disconnected')
 
-
 if __name__ == '__main__':
-    sock.run(app, host='127.0.0.1', port=8078, debug=True)
-    if 'camera' in cache:
-        del cache['camera']
+    sock.run(app, host='127.0.0.1', port=8079, debug=True)
